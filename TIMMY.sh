cc6ed4f29f1f8bc6f66c3dab9092077b2cf359bb
Hangyeol <37773452+gksruf2@users.noreply.github.com>
2022-10-11 14:18:16 +0900

    Add files via upload

---
+++ TIMMY.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#SBATCH --job-name my_capston
+#SBATCH --gress-hpu:1
+#SBATCH --cpus-per-gpu=4
+#SBATCH --time 1-0
+#SBATCH --mem-per-gpu=3G
+#SBATCH --partition batch_ugrad
+#SBATCH -x ai10
+#SBATCH -o slurm/logs/slurm-%A-%x.out
+
+python hello.py
+
+exit 0
